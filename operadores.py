4ee7bbc9b3436eab247b1fcc1b7336bff5b5b890
marcos <marcossistemas.ac@gmail.com>
2022-08-11 17:30:39 -0500

    #2-adicionar operadores básicos (subtração, multiplicação e divisão)

---
+++ operadores.py
@@ -0,0 +1,16 @@
+#subtração 
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+print('{} - {} ='.format(number_1, number_2))
+print(number_1 - number_2)
+
+#multiplicação 
+
+print('{} * {} ='.format(number_1, number_2))
+print(number_1 * number_2)
+
+#divisão 
+
+print('{} / {} ='.format(number_1, number_2))
+print(number_1 / number_2)
